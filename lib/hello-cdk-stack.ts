76a00507cfd13df9d7e7c8b21e2f2c04c9715c55
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-10-25 12:45:28 +0530

    add customEvent

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -6,12 +6,12 @@ export class NewRelicExampleCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     // Add latest New Relic Lambda layer ARN from https://layers.newrelic-external.com
-    const NewReliclayerArn = 'arn:aws:lambda:us-east-1:451483290750:layer:NewRelicNodeJS20X:39';
+    const NewReliclayerArn = 'arn:aws:lambda:us-east-1:451483290750:layer:NewRelicNodeJS20X:40';
     const myFunction = new lambda.Function(this, "NewRelicExampleLambda", {
       runtime: lambda.Runtime.NODEJS_20_X, 
       // Update functions handler to point to the New Relic Lambda wrapper
       handler: "newrelic-lambda-wrapper.handler",
-      code: lambda.Code.fromAsset('lib/lambda-runtime-code'),
+      code: lambda.Code.fromAsset('lib/newrelic-lambda-code'),
       layers: [lambda.LayerVersion.fromLayerVersionArn(this, 'NewRelicLayer', NewReliclayerArn)],
       environment: {
         // Set the NEW_RELIC_LAMBDA_HANDLER environment variable to the path of your initial handler.
